4f0ebcb09c5d132aae93d78820feef6beea9edf7
DmitriiDudarow <dudarow.d@yandex.ru>
2024-09-27 16:22:23 +0500

    Dudarow Task 4

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,11 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Point scnd = new Point(3,3);
+        Point frst = new Point(0,0);
+        Point trd = new Point(1,1);
+        Line line = new Line(frst,scnd);
+        System.out.println(line.isCollinearLine(trd));
+        System.out.println(line);
     }
 }
